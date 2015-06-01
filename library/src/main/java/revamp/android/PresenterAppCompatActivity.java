dc96212b4db020c3a88df55dac5880462657a0f8
Nacho L <nacho@nlopez.io>
2015-06-01 11:12:35 +0100

    Added the CallSuper annotations for the presenter aware activities and fragment

--- library/src/main/java/revamp/android/PresenterAppCompatActivity.java
+++ library/src/main/java/revamp/android/PresenterAppCompatActivity.java
@@ -1,6 +1,7 @@
 package revamp.android;
 
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.support.v7.app.AppCompatActivity;
 
 import revamp.android.delegates.PresenterActivityDelegate;
@@ -17,61 +18,70 @@ public abstract class PresenterAppCompatActivity<P extends Presenter<V>, V exten
     protected PresenterActivityDelegate<V> delegate;
 
     @Override
+    @CallSuper
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getPresenterDelegate().onCreate(savedInstanceState);
     }
 
     @Override
-
+    @CallSuper
     public void onDestroy() {
         super.onDestroy();
         getPresenterDelegate().onDestroy();
     }
 
     @Override
+    @CallSuper
     public void onStart() {
         super.onStart();
         getPresenterDelegate().onStart();
     }
 
     @Override
+    @CallSuper
     public void onStop() {
         super.onStop();
         getPresenterDelegate().onStop();
     }
 
     @Override
+    @CallSuper
     public void onResume() {
         super.onResume();
         getPresenterDelegate().onResume();
     }
 
     @Override
+    @CallSuper
     public void onPause() {
         super.onPause();
         getPresenterDelegate().onPause();
     }
 
     @Override
+    @CallSuper
     public void onRestart() {
         super.onRestart();
         getPresenterDelegate().onRestart();
     }
 
     @Override
+    @CallSuper
     public void onContentChanged() {
         super.onContentChanged();
         getPresenterDelegate().onContentChanged();
     }
 
     @Override
+    @CallSuper
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         getPresenterDelegate().onSaveInstanceState(outState);
     }
 
     @Override
+    @CallSuper
     public void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         getPresenterDelegate().onPostCreate(savedInstanceState);
--- library/src/main/java/revamp/android/PresenterFragment.java
+++ library/src/main/java/revamp/android/PresenterFragment.java
@@ -3,6 +3,7 @@ package revamp.android;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.view.View;
 
@@ -20,72 +21,84 @@ public abstract class PresenterFragment<P extends Presenter<V>, V extends ViewCo
     protected PresenterFragmentDelegate<V> delegate;
 
     @Override
+    @CallSuper
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getPresenterDelegate().onCreate(savedInstanceState);
     }
 
     @Override
+    @CallSuper
     public void onDestroy() {
         super.onDestroy();
         getPresenterDelegate().onDestroy();
     }
 
     @Override
+    @CallSuper
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getPresenterDelegate().onViewCreated(view, savedInstanceState);
     }
 
     @Override
+    @CallSuper
     public void onDestroyView() {
         super.onDestroyView();
         getPresenterDelegate().onDestroyView();
     }
 
     @Override
+    @CallSuper
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getPresenterDelegate().onActivityCreated(savedInstanceState);
     }
 
     @Override
+    @CallSuper
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         getPresenterDelegate().onAttach(activity);
     }
 
     @Override
+    @CallSuper
     public void onDetach() {
         super.onDetach();
         getPresenterDelegate().onDetach();
     }
 
     @Override
+    @CallSuper
     public void onStart() {
         super.onStart();
         getPresenterDelegate().onStart();
     }
 
     @Override
+    @CallSuper
     public void onStop() {
         super.onStop();
         getPresenterDelegate().onStop();
     }
 
     @Override
+    @CallSuper
     public void onResume() {
         super.onResume();
         getPresenterDelegate().onResume();
     }
 
     @Override
+    @CallSuper
     public void onPause() {
         super.onPause();
         getPresenterDelegate().onPause();
     }
 
     @Override
+    @CallSuper
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         getPresenterDelegate().onSaveInstanceState(outState);
